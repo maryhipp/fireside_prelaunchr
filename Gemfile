3bf4c41902c49fbcd0f545401b121fbf93bab25b
maryhipp <maryhipp@gmail.com>
2015-03-24 09:36:34 -0400

    facebooks hare

--- Gemfile
+++ Gemfile
@@ -52,4 +52,3 @@ gem 'rails_12factor', group: :production
 # gem 'debugger'
 
 
-ruby '2.0.0'
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,7 +63,17 @@ Prelaunchr::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
-  config.action_mailer.default_url_options = { :host => 'www.example.com' }
+  config.action_mailer.default_url_options = { :host => 'portal.herokuapp.com' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+  address:              'smtp.gmail.com',
+  port:                 587,
+  domain:               'gmail.com',
+  user_name:            'mary@cobblehilldigital.com',
+  password:             'gonefishing77',
+  authentication:       'plain',
+  enable_starttls_auto: true  }
+  config.action_mailer.raise_delivery_errors = true
 
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
