86708f0f4a7c4f511e3716052dd9e16aa00b2d10
maryhipp <maryhipp@gmail.com>
2015-03-30 17:52:29 -0400

    styling welcome email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,7 +63,7 @@ Prelaunchr::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
-  config.action_mailer.default_url_options = { :host => 'https://pure-citadel-4400.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'http://pure-citadel-4400.herokuapp.com' }
 
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
