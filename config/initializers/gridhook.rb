9885ba885451574afaa2f2f51be0be403a8b6f28
maryhipp <maryhipp@gmail.com>
2015-03-31 13:58:34 -0400

    bounce

--- config/initializers/gridhook.rb
+++ config/initializers/gridhook.rb
@@ -1,5 +1,9 @@
 Gridhook.configure do |config|
   # The path we want to receive events
   config.event_receive_path = '/'
-  config.event_processor = EventProcessor.new
+
+  config.event_processor = proc do |event|
+    # event is a Gridhook::Event object
+    EmailEvent.create! event.attributes
+  end
 end
