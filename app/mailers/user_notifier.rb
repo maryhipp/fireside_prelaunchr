fb78d82d161f792643cbd8196598db5cd1df0b2f
maryhipp <maryhipp@gmail.com>
2015-03-31 11:06:54 -0400

    email share buttons

--- app/mailers/user_notifier.rb
+++ app/mailers/user_notifier.rb
@@ -3,6 +3,7 @@ class UserNotifier < ActionMailer::Base
 
   def send_signup_email(user)
     @user = user
+    @twitter_message = "Excited for @FProvisions to launch. I’ll be headed #outdoors this summer with great meals, for free!"
     mail( :to => @user.email,
     :subject => 'Thanks for signing up for Fireside Provisions' )
   end
