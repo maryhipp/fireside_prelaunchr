49e1bc821bf0a1c80d2bde3e7a5d990a426b3e0c
maryhipp <maryhipp@gmail.com>
2015-03-31 13:53:20 -0400

    bounce

--- app/models/event_processor.rb
+++ app/models/event_processor.rb
@@ -1,11 +1,11 @@
 class EventProcessor
 
-  def call(gridhook_event)
-    email = gridhook_event.email
-    if gridhook_event.event = "bounce"
+  def call(event)
+    email = event.email
+    if event.event = "bounce"
     	user = User.find_by_email(email)
     	user.destroy
     end
   end
-  
+
 end
